a4f639ae6a0a5c377f21975918f5b881331deeee
ngonhan2k5 <ngonhan2k5@gmail.com>
2020-05-13 16:07:45 -0500

    Release client

--- probuild/service-worker.js
+++ probuild/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f00a110c6a2e0aea884a33111844907a.js"
+  "/probuild/precache-manifest.dbe025ba3320f8871cc0d03882176896.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/probuild/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
